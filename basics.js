18e64ead5366cd0195fb07c9ef091ea9810ce634
Rishabh Singh <rishabhsingh50689@gmail.com>
2023-03-12 20:16:11 +0530

    Console.log

--- basics.js
+++ basics.js
@@ -1,4 +1,14 @@
 // alert is used as a pop up box 
 alert("Hello !")
 
+// input 
+prompt("Enter your name?") // asking user to take input
+
 // data types 
+var Myname = "Rishabh"    // string
+var age = 19            // integer
+var isMarried = false   // boolean
+
+// concatenating 
+console.log("My name is:\t" + Myname );
+
