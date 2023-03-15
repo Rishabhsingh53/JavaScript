8f9ff068724e231ab7a4aa74370d936240e3dda3
Rishabh Singh <rishabhsingh50689@gmail.com>
2023-03-15 20:47:06 +0530

    Operators and strings

--- basics.js
+++ basics.js
@@ -4,11 +4,24 @@ alert("Hello !")
 // input 
 prompt("Enter your name?") // asking user to take input
 
+// variables are created using var keyword
+
 // data types 
-var Myname = "Rishabh"    // string
+var Myname = "Rishabh Singh"    // string
 var age = 19            // integer
 var isMarried = false   // boolean
 
-// concatenating 
-console.log("My name is:\t" + Myname );
+// strings 
+var greet = "Hello"
+console.log(`the length of the ${greet} is ${greet.length}`); // length 
+console.log(greet + "there," + Myname); // concatenation
+console.log(Myname.slice(0,8))          // slicing
+console.log(Myname.toUpperCase())
+console.log(Myname.toLowerCase())
 
+// operators 
+console.log(2 + 3)      // addition
+console.log(10 - 4)     // subtraction
+console.log(4 * 5)      // multiplication 
+console.log(5 / 1 )     // division operator
+console.log(10 % 3)     // modulo 
