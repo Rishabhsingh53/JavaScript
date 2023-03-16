1f02fabe88cb4d0af4255ea8063b9fde08b11d04
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-16 21:11:15 +0530

    Initial commit

---
+++ Intermediate JavaScript/99 bottles of beer/solution.js
@@ -0,0 +1,7 @@
+count = 99 
+
+while (count >= 1 ) {
+    console.log(`${count} bottles of beer on the wall, ${count} bottles of beer. Take one down, pass it around, ${--count} bottles of beer on the wall.`)
+}
+
+console.log(`No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of the beer on the wall`)
