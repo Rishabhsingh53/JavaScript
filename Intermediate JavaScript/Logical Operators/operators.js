7465309c46fdee523125509647bfff3f50e5088a
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-16 19:52:33 +0530

    Initial commit

---
+++ Intermediate JavaScript/Logical Operators/operators.js
@@ -0,0 +1,28 @@
+/*
+&&      AND 
+||      OR
+!       NOT
+*/
+
+// AND
+if (true && false) {
+    console.log("condition 1 of AND")
+} 
+if (false && false) {
+    console.log("condition 2 of AND")
+} 
+if (true && true) {                        
+    console.log("condition 3 of AND")
+}
+
+// OR 
+if (true || true) {
+    console.log("condition 1 of OR")
+}
+if (true || false) {
+    console.log("condition 2 of OR")
+}
+if (false || false) {
+    console.log("condition 3 of OR")
+}
+
