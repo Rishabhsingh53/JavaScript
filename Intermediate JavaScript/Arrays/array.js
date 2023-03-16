4fa45ede6b314efbf2390d3705010144d4d2beee
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-16 20:53:37 +0530

    Push and pop

--- Intermediate JavaScript/Arrays/array.js
+++ Intermediate JavaScript/Arrays/array.js
@@ -9,3 +9,11 @@ console.log("Length of the array is:\t"+arr.length)
 
 // to check if the array contains a particular item use includes method
 console.log(arr.includes(5));
+
+// to add elements in the array 
+arr.push(7);
+console.log(arr)
+
+// to remove the last element from the array 
+arr.pop()
+console.log(arr)
