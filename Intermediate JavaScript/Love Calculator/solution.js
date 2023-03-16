9a99253d29fe6da64183884e40e756a7d7be3a13
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-16 12:41:44 +0530

    Initial commit

---
+++ Intermediate JavaScript/Love Calculator/solution.js
@@ -0,0 +1,5 @@
+person1 = prompt("Enter the name of the boy")
+person2 = prompt("Enter the name of the girl")
+
+n = Math.random()
+alert(`${person1} loves ${person2} and their love score is : ${Math.floor(n*100)+1}%`)
