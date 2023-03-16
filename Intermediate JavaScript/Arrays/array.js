bc23f105352a91b453133ce49ddba12b9d3152d1
Rishabhsingh53 <rishabhsingh50689@gmail.com>
2023-03-16 20:21:54 +0530

    Initial commit

---
+++ Intermediate JavaScript/Arrays/array.js
@@ -0,0 +1,11 @@
+// This is an array. Array is a collection of similar items.
+arr = [1,2,3,4,5,6];
+
+// accessing items from array
+console.log("The first item in the array is:\t"+arr[0])
+
+// length of the array
+console.log("Length of the array is:\t"+arr.length)
+
+// to check if the array contains a particular item use includes method
+console.log(arr.includes(5));
